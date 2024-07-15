93859201c285a88e3c0f7a50c6c59ff691e55072
mutha <mutha@192.168.1.34>
2024-07-15 23:43:50 +0530

    dev-bsnl recharge done

--- MavenProj01/src/main/java/com/nt/service/Recharge.java
+++ MavenProj01/src/main/java/com/nt/service/Recharge.java
@@ -4,7 +4,13 @@ public class Recharge {
 	public String rechargeMobiles(long mobileNo) {
 		return "Recharge Mobiles are done";
 	}
+
 	public String airtelRecharge() {
 		return "airtel recharge is done";
+	}
+	
+	public String bsnlRecharg() {
+		return "bsnl recharge done";
+
 	}
 }
