4f2d88aa6f553782ff9493f4dbf90a2c763d66e2
mutha <mutha@192.168.1.34>
2024-07-15 23:41:29 +0530

    TL-Airtel Recharge is Done

--- MavenProj01/src/main/java/com/nt/service/Recharge.java
+++ MavenProj01/src/main/java/com/nt/service/Recharge.java
@@ -4,4 +4,7 @@ public class Recharge {
 	public String rechargeMobiles(long mobileNo) {
 		return "Recharge Mobiles are done";
 	}
+	public String airtelRecharge() {
+		return "airtel recharge is done";
+	}
 }
